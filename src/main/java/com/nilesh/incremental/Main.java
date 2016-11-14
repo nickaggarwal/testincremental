1b39d0ab2213cef4239090363d24c238543451da
Nilesh <nickaggarwal>
2016-11-14 19:39:26 +0530

    Starting Project

---
+++ src/main/java/com/nilesh/incremental/Main.java
@@ -0,0 +1,12 @@
+package com.nilesh.incremental;
+
+/**
+ * Created by Nilesh on 14/11/16.
+ */
+public class Main {
+
+    public static void main(String args[])
+    {
+        System.out.print("Hello World");
+    }
+}
